ec36b964a4b53ca913a9106e96f6c3169df3474d
sinchang <sinchangwen@gmail.com>
2018-02-05 10:43:35 +0800

    chore: update component name

---
+++ packages/button/src/Button.vue
@@ -0,0 +1,45 @@
+<template>
+  <button 
+    :class="{
+      'btn': true, 
+      [`btn-${type}`]: type, 
+      [`btn-${size}`]: size, 
+      'btn-block': block,
+      'active': active, 
+      'loading': loading, 
+      'disabled': disabled }">
+    <slot/>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'FoButton',
+    props: {
+      type: {
+        type: String,
+        default: ''
+      },
+      size: {
+        type: String,
+        default: ''
+      },
+      block: {
+        type: Boolean,
+        default: false
+      },
+      active: {
+        type: Boolean,
+        default: false
+      },
+      loading: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
---
+++ packages/icon/src/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <i 
+    :class="{
+      'icon': true,
+      [`icon-${type}`]: type,
+      [`icon-${size}`]: size }" />
+</template>
+
+<script>
+  export default {
+    name: 'FoIcon',
+    props: {
+      type: {
+        type: String,
+        default: ''
+      },
+      size: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
